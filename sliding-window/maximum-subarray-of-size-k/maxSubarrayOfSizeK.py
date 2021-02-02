e1cdf291034aaed995dbdd7b39e7e87e5f267ef0
Yasoob Rasheed <yasoob.f.rasheed@gmail.com>
2021-02-01 18:51:56 -0800

    :tada: initial commit

---
+++ sliding-window/maximum-subarray-of-size-k/maxSubarrayOfSizeK.py
@@ -0,0 +1,11 @@
+def maxSubarrayOfSizeK(arr, k):
+  maxSum = 0
+  for i in range(0, k):
+    maxSum += arr[i]
+
+  currSum = maxSum
+  for i in range(k, len(arr)):
+    currSum = currSum - arr[i - k] + arr[i]
+    maxSum = max(maxSum, currSum)
+  
+  return maxSum
