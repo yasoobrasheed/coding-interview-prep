b8e7da71a0b48eb15c83a07fd3c6d8d4f4ca7f49
Yasoob Rasheed <yasoob.f.rasheed@gmail.com>
2021-02-09 13:04:06 -0600

    :pencil2: adding topSort shell step to topSort algorithm

--- topological-sort/topological-sort/topSort.py
+++ topological-sort/topological-sort/topSort.py
@@ -2,7 +2,6 @@ from collections import defaultdict
 
 
 class Graph:
-    # constructor
     def __init__(self, vertices):
         self.graph = defaultdict(list)
         self.V = vertices
@@ -10,26 +9,51 @@ class Graph:
         for i in range(0, vertices):
             self.visited[i] = False
 
-    # create graph
     def addEdge(self, u, v):
         self.graph[u].append(v)
 
-    # graph depth first search
-    def dfs(self, u, graph):
-        print(u)
+    def topSort(self):
+        order = [0] * 13
+        endIndex = self.V - 1
+
+        for i in range(0, self.V):
+            if (self.visited[i] == False):
+                endIndex = self.dfs(i, order, endIndex)
+
+        return order
+
+    def dfs(self, u, order, endIndex):
         self.visited[u] = True
 
         for v in self.graph[u]:
             if (self.visited[v] == False):
-                self.dfs(v, graph)
+                endIndex = self.dfs(v, order, endIndex)
+
+        order[endIndex] = u
+        return endIndex - 1
 
 
 def __main__():
-    g = Graph(6)
-    g.addEdge(0, 1)
+    g = Graph(13)
+    g.addEdge(0, 3)
     g.addEdge(1, 3)
-    g.addEdge(3, 2)
-    g.addEdge(2, 5)
-    g.addEdge(2, 4)
+    g.addEdge(2, 0)
+    g.addEdge(2, 1)
+    g.addEdge(3, 7)
+    g.addEdge(3, 6)
+    g.addEdge(4, 0)
+    g.addEdge(4, 3)
     g.addEdge(4, 5)
-    g.dfs(0, g)  # 0 1 3 2 5 4
+    g.addEdge(5, 10)
+    g.addEdge(5, 9)
+    g.addEdge(6, 8)
+    g.addEdge(7, 8)
+    g.addEdge(7, 9)
+    g.addEdge(8, 11)
+    g.addEdge(9, 11)
+    g.addEdge(9, 12)
+    arr = g.topSort()
+    letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M']
+    for num in arr:
+        print(letters[num], end=" ")
+    # E F K C B A D G H J M I L
